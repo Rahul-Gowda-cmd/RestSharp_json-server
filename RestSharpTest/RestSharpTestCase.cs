465cf39c52ad49e6eb06547af2004cf69823e93a
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-09-12 14:09:35 +0530

    feat: Writing Restsharp test using json server

---
+++ RestSharpTest/RestSharpTestCase.cs
@@ -0,0 +1,93 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Collections.Generic;
+
+namespace RestSharpTest
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double Salary { get; set; }
+    }
+    [TestClass]
+    public class RestSharpTestCase
+    {
+        RestClient client;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            client = new RestClient("http://localhost:4000");
+        }
+        private IRestResponse getEmployeeList()
+        {
+            RestRequest request = new RestRequest("/employees/list", Method.GET);
+
+            //act
+
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+        [TestMethod]
+        public void onCallingGETApi_ReturnEmployeeList()
+        {
+            IRestResponse response = getEmployeeList();
+
+            //assert
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
+            List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+            Assert.AreEqual(8, dataResponse.Count);
+            foreach (var item in dataResponse)
+            {
+                System.Console.WriteLine("id: " + item.id + "Name: " + item.name + " ,Salary: " + item.Salary);
+            }
+        }
+        [TestMethod]
+        public void givenEmployee_OnPost_ShouldReturnAddedEmployee()
+        {
+            RestRequest request = new RestRequest("/employees", Method.POST);
+            JObject jObjectbody = new JObject();
+            jObjectbody.Add("name", "Clark");
+            jObjectbody.Add("Salary", "15000");
+            request.AddParameter("application/json", jObjectbody, ParameterType.RequestBody);
+
+            //act
+            IRestResponse response = client.Execute(request);
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.Created);
+            Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Clark", dataResponse.name);
+            Assert.AreEqual(15000, dataResponse.Salary);
+
+        }
+        [TestMethod]
+        public void givenEmployeeSalary_OnUpdate_ShouldReturnAddedEmployee()
+        {
+            RestRequest request = new RestRequest("/employees/11", Method.PUT);
+            JObject jObjectbody = new JObject();
+
+            jObjectbody.Add("name", "Clark");
+            jObjectbody.Add("Salary", "30000");
+            request.AddParameter("application/json", jObjectbody, ParameterType.RequestBody);
+
+            //act
+            var response = client.Execute(request);
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
+            Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Clark", dataResponse.name);
+            Assert.AreEqual(30000, dataResponse.Salary);
+
+        }
+        [TestMethod]
+        public void givenEmployeeID_SholudDeleteEmployee()
+        {
+            RestRequest request = new RestRequest("/employees/5", Method.DELETE);
+            IRestResponse response = client.Execute(request);
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
+
+        }
+    }
+}
